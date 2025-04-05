7b10ea8f025526dbf14d9d8f52f8e4001bf38037
Pedro Rien <pedrorrien@gmail.com>
2025-04-04 21:56:26 -0300

    Aula 04/04/25

--- greating-service/src/main/java/br/edu/atitus/greating_service/GreatingController.java
+++ greating-service/src/main/java/br/edu/atitus/greating_service/GreatingController.java
@@ -1,16 +1,27 @@
 package br.edu.atitus.greating_service;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/greating-service")
 public class GreatingController {
 
-    @GetMapping
-    public String getGreeting() {
-        return "Hello World";
+    @Value("${greating-service.greating}")
+    private String greating;
+
+    @Value("${greating-service.greating}")
+    private String nameDefault;
+
+    @GetMapping({"","/","/{namePath}"})
+    public ResponseEntity<String> getGreating(@PathVariable(required = false) String namePath,
+                                              @RequestParam(required = false) String name) {
+        if (name == null)
+            name = namePath != null ? namePath : "Word";
+
+        String returnText = String.format("%s %s!", greating, name);
+        return ResponseEntity.ok(returnText);
     }
 
 }
